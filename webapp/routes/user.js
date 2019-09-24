01287b6fed8d72237db6c810f368d17a5f3eaa65
Janhavi Bhalerao <bhalerao.j@husky.neu.edu>
2019-09-23 22:28:40 -0400

    updating response

--- webapp/routes/user.js
+++ webapp/routes/user.js
@@ -15,34 +15,43 @@ router.post('/', (req, res, next) => {
         let last_name = req.body.last_name;
         let password = req.body.password;
         let email_address = req.body.email_address;
-        
         let salt = bcrypt.genSaltSync(saltRounds);
         let hashedPassword = bcrypt.hashSync(password, salt);
-        
+
         if (first_name != null && last_name != null && password != null && email_address != null && validator.validate(password) == true && emailValidator.validate(email_address) == true) {
+            const id = uuid();
+            const account_created = moment().format('YYYY-MM-DD HH:mm:ss');
+            const account_updated = moment().format('YYYY-MM-DD HH:mm:ss');
+
             mysql.query('insert into RMS.User(`id`,`first_name`,`last_name`,`password`,`email_address`,`account_created`,`account_updated`)values(?,?,?,?,?,?,?)',
-                [uuid(),first_name,last_name,hashedPassword,email_address,moment().format('YYYY-MM-DD HH:mm:ss'),moment().format('YYYY-MM-DD HH:mm:ss')], (err, result) => {
-                if (err) {
-                    console.log(err);
-                    res.status(409).json({ msg: ` Email: ${email_address} already exists!` });
-                }
-                else {
-                    res.status(201).json(`User created successfully!Created email: ${email_address}`);
-                }
-            });
+                [id, first_name, last_name, hashedPassword, email_address, account_created, account_updated], (err, result) => {
+                    if (err) {
+                        console.log(err);
+                        res.status(400).json({ msg: ` Email: ${email_address} already exists!` });
+                    }
+                    else {
+                        //res.status(201).json(`User created successfully!Created email: ${email_address}`);
+                        res.status(201).json({
+                            id: id,
+                            first_name: first_name,
+                            last_name: last_name,
+                            email_address: email_address,
+                            account_created: account_created,
+                            account_updated: account_updated
+                        });
+                    }
+                });
         }
-        else if (first_name == null || last_name == null || password == null || email_address == null ) {
+        else if (first_name == null || last_name == null || password == null || email_address == null) {
             res.status(400).json({ msg: 'Please enter all details!' });
         }
         else if (emailValidator.validate(email_address) == false) {
-            res.status(400).json({ msg: `${email_address} is not a valid email` });
+            res.status(400).json({ msg: `${email_address} is not a valid email!` });
         }
-
         else {
             res.status(400).json({
-                msg: 'password must be atleast 9 letters \
-        and should contain an uppercase, lowercase, digits and symbols. Also should \
-        not contain spaces'});
+                msg: 'Password must be: atleast 8 letters & should contain an uppercase, a lowercase, a digit & a symbol! No spaces allowed'
+            });
         }
     }
     else {
