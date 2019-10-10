2eedcbd82573dda00db2da23d3b07981e76c0ce6
Janhavi Bhalerao <bhalerao.j@husky.neu.edu>
2019-10-10 12:17:45 -0400

    multiple VPCs

---
+++ infrastructure/aws/cloudformation/README.md
@@ -0,0 +1,2 @@
+chmod +x csye6225-aws-cf-create-stack.sh
+./csye6225-aws-cf-create-stack.sh
--- infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-terminate-stack.sh
@@ -5,12 +5,15 @@ read s_name
 
 echo "Stack:$s_name - Termination in progress..."
 
-aws cloudformation delete-stack --stack-name $s_name
+StackList=$(aws cloudformation list-stack-resources --stack-name $s_name)
+
 if [ $? -ne "0" ]
-then
-  echo "Stack:$s_name - Does not exist!"
+ then
+  echo "$s_name Stack does not exist!"
   exit 1
 fi
+
+aws cloudformation delete-stack --stack-name $s_name
 
 aws cloudformation wait stack-delete-complete --stack-name $s_name
 
