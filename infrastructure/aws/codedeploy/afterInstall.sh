af331c290fe553a83c019683a377b9c3de3be692
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 04:54:34 -0500

    cd change

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -3,8 +3,8 @@ pwd
 whoami
 aws configure set default.region us-east-1
 aws configure list
-chmod -R 755 /home/centos/webapp
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/infrastructure/aws/codedeploy/cloudwatch-config.json -s
 cd /home/centos/webapp/
 pwd
-npm install
-npm install pm2 -g
+sudo npm install
+sudo npm install pm2 -g
