c66d668e878de5ad694147d71a342cdd93710c6a
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 02:49:23 -0500

    afterinstall modified

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source /home/centos/.bash_profile
 pwd
 whoami
 aws configure set default.region us-east-1
 aws configure list
+chmod -R 755 /home/centos/webapp
 cd /home/centos/webapp/
 pwd
 npm install
