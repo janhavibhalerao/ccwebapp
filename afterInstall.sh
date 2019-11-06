99ea246b37ff2d308e9c947336f2fc180d924d7c
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 23:14:45 -0500

    check1

--- afterInstall.sh
+++ afterInstall.sh
@@ -6,8 +6,9 @@ aws configure list
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
 pwd
 cd /home/centos/webapp
+echo "check1" > .test
 sudo npm install
 sudo nohup npm start >> app.log 2>&1 &
-
+echo "check2" > .test
 #pm2 update
 #pm2 start server.js
