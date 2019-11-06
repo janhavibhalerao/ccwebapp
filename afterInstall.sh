5e05c8de8ea2f2594f71e2f039b3f1e5ccb16841
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 23:23:56 -0500

    check2

--- afterInstall.sh
+++ afterInstall.sh
@@ -5,10 +5,12 @@ aws configure set default.region us-east-1
 aws configure list
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
 pwd
-cd /home/centos/webapp
+cd /home/centos
+sudo chmod 777 webapp
+cd /webapp
 echo "check1" > .test
-sudo npm install
-sudo nohup npm start >> app.log 2>&1 &
-echo "check2" > .test
+npm install
+nohup npm start >> app.log 2>&1 &
+echo "check2" >> .test
 #pm2 update
 #pm2 start server.js
