65bd0ad88c252e6bb05762251551d3824333a0b1
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 22:26:07 -0500

    wrong commit

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pwd
-whoami
+whoa
 aws configure set default.region us-east-1
 aws configure list
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
@@ -8,7 +8,7 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-c
 sudo chown centos /home/centos/webapp/
 pwd
 cd /home/centos/webapp
-sudo npm install
+sudo npm instal
 sudo nohup npm start >> app.log 2>&1 &
 
 #pm2 update
