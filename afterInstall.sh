7bbcf5d5675de9b3a7cdee0d11bf724693ec0031
ravi kumar <ravikumar.pilla479@gmail.com>
2019-11-05 22:37:45 -0500

    deploytest10

--- afterInstall.sh
+++ afterInstall.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 pwd
-whoa
+whoami
 aws configure set default.region us-east-1
 aws configure list
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/centos/cloudwatch-config.json -s
-
-sudo chown centos /home/centos/webapp/
 pwd
 cd /home/centos/webapp
-sudo npm instal
+sudo npm install
 sudo nohup npm start >> app.log 2>&1 &
 
 #pm2 update
