c0f4370c7ed8f7a0e6d0095cecce0ae95bcf35df
Janhavi Bhalerao <46508254+janhavibhalerao@users.noreply.github.com>
2020-01-21 21:11:06 -0500

    update config

--- webapp/config/config.js
+++ webapp/config/config.js
@@ -1,27 +1,4 @@
-// const config = { 
-//     aws: { 
-//         aws_secret_key: 'MnP1XI/WasYCx1aDPdG6Wcu9LgiZeEoroTy9q94O', 
-//         aws_access_key: 'AKIAQE3UTL67KRCBHN5T', 
-//         aws_region: 'us-east-1', 
-//         s3_bucket_name: 'webapp.janhavibhalerao.me' 
-//     }, 
-//     mysql: { 
-//         host: "localhost", 
-//         user: "root", 
-//         password: "Admin@123", 
-//         database: "RMS" 
-//     }, 
-//     db: {
-//         host: "csye6225-fall2019.cebvhwtrbewj.us-east-1.rds.amazonaws.com",
-//         user: "dbuser",
-//         password: "Admin_123",
-//         port: "3306",
-//         database: "csye6225"
 
-//     },
-//         port: 3000 
-//     }; 
-// module.exports = config;
 
 module.exports = function() {
     return {
